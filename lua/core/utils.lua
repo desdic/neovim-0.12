12b693cd41eedd822354b989ff64806ec02b202e
Kim Gert Nielsen <kn@insecurity.dk>
2025-11-21 07:52:42 +0100

    Remove unused function

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -5,15 +5,6 @@ M.has_plugin = function(plugin)
     return pcall(require, plugin)
 end
 
-M.is_top_empty_file = function()
-    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    local _, col = vim.api.nvim_win_get_cursor(0)
-    if #lines <= 1 and col == 2 then
-        return true
-    end
-    return false
-end
-
 M.pack_clean = function()
     local unused_plugins = {}
 
