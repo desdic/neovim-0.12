ec6709022517886f8eca976f95c725f78cbc3509
Kim Gert Nielsen <kn@insecurity.dk>
2025-12-07 21:22:54 +0100

    Move loading to autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -113,6 +113,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
+-- Set up LSP servers.
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    once = true,
+    callback = function()
+        -- Extend neovim's client capabilities with the completion ones.
+        vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities(nil, true) })
+
+        local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+            :map(function(file)
+                return vim.fn.fnamemodify(file, ":t:r")
+            end)
+            :totable()
+        vim.lsp.enable(servers)
+    end,
+})
+
 vim.api.nvim_create_autocmd("BufReadPost", {
     group = vim.api.nvim_create_augroup("UserBufConfig", {}),
     callback = function(ev)
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,42 +1,33 @@
-local configs = {}
-
-for _, v in ipairs(vim.api.nvim_get_runtime_file("lsp/*", true)) do
-	local name = vim.fn.fnamemodify(v, ":t:r")
-	configs[name] = true
-end
-
-vim.lsp.enable(vim.tbl_keys(configs))
-
 vim.diagnostic.config({
-	update_in_insert = true,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focusable = false,
-		style = "minimal",
-		border = "rounded",
-		source = "if_many",
-		header = "",
-		prefix = "",
-	},
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = " ",
-			[vim.diagnostic.severity.WARN] = " ",
-			[vim.diagnostic.severity.INFO] = " ",
-			[vim.diagnostic.severity.HINT] = "󰠠 ",
-		},
-		texthl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-		},
-		numhl = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.INFO] = "",
-			[vim.diagnostic.severity.HINT] = "",
-		},
-	},
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "if_many",
+        header = "",
+        prefix = "",
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "󰠠 ",
+        },
+        texthl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+        },
+    },
 })
