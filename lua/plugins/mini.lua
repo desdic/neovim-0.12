78a979b3c1bf87c6984172874f9420c6493d71db
Kim Gert Nielsen <kn@insecurity.dk>
2025-12-07 20:32:56 +0100

    Remove blink-pair and mini-surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,6 @@ vim.pack.add({
     { src = "https://github.com/nvim-mini/mini.surround" },
 }, { confirm = false })
 
--- vim.defer_fn(function()
 require("mini.tabline").setup({
     format = function(buf_id, label)
         local suffix = vim.bo[buf_id].modified and "+" or ""
@@ -12,7 +11,6 @@ require("mini.tabline").setup({
     end,
     tabpage_section = "right",
 })
--- end, 100)
 
 vim.defer_fn(function()
     require("mini.ai").setup({
@@ -22,18 +20,6 @@ vim.defer_fn(function()
             inside_next = "",
             around_last = "",
             inside_last = "",
-        },
-    })
-
-    require("mini.surround").setup({
-        mappings = {
-            add = "gsa", -- Add surrounding in Normal and Visual modes
-            delete = "gsd", -- Delete surrounding
-            find = "gsf", -- Find surrounding (to the right)
-            find_left = "gsF", -- Find surrounding (to the left)
-            highlight = "gsh", -- Highlight surrounding
-            replace = "gsr", -- Replace surrounding
-            update_n_lines = "gsn", -- Update `n_lines`
         },
     })
 end, 300)
